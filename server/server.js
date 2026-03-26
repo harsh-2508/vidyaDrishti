00201518fc7c774c3d6eca96f06d336116db83a5
Harshvardhan Singh Thakur <hvst032003@gmail.com>
2026-03-26 23:00:57 +0530

    vercel error-2

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ connectDB();
 // --- Middleware ---
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173', // Uses Vercel URL in prod
+  origin: process.env.CLIENT_URL || 'https://vidya-drishti.vercel.app', // Uses Vercel URL in prod
   credentials: true // Important if you use cookies/sessions
 }));
 
